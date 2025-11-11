69bbe66dde000bb1ab66770a112aed1412875eee
HMLeeSoundcat <hm.lee@soundcat.com>
2025-11-11 14:48:29 +0900

    빌드옵션 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
   server: {
-    allowedHosts: ['5173.hmlee.me']
-  }
-})
+    allowedHosts: ["5173.hmlee.me"],
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        // JS, CSS, 이미지 등 자산의 폴더 경로 지정
+        assetFileNames: "apps/b2b_write/assets/[name]-[hash][extname]",
+        chunkFileNames: "apps/b2b_write/assets/[name]-[hash].js",
+        entryFileNames: "apps/b2b_write/assets/[name]-[hash].js",
+      },
+    },
+  },
+});
