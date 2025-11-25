15312f519462e806acab98073edf8496d9b8524c
HMLeeSoundcat <hm.lee@soundcat.com>
2025-11-25 16:02:45 +0900

    빌드 디렉터리 설정 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,17 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), visualizer({ filename: "stats.html", gzipSize: true })],
   server: {
     allowedHosts: ["5173.hmlee.me"],
   },
   build: {
     rollupOptions: {
-      output: {
-        // JS, CSS, 이미지 등 자산의 폴더 경로 지정
-        assetFileNames: "apps/b2b_write/assets/[name]-[hash][extname]",
-        chunkFileNames: "apps/b2b_write/assets/[name]-[hash].js",
-        entryFileNames: "apps/b2b_write/assets/[name]-[hash].js",
-      },
+      output: {},
     },
+    assetsDir: "apps/b2b_write/assets",
   },
 });
