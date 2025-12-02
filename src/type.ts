39577738b00204bc9ef3e3e2f38fb781d1c783b4
HMLeeSoundcat <hm.lee@soundcat.com>
2025-12-02 20:49:38 +0900

    타입 분리 및 보강, 마진설정값 반영

--- src/type.ts
+++ src/type.ts
@@ -1,14 +1,16 @@
 export type 배송형태종류타입 = (typeof 배송형태종류)[number];
 
-export type 품목리스트항목타입 = {
+export interface 품목리스트항목타입 {
   uuid: string;
   productInfo: 제품정보타입;
   deliveryInfo: 배송정보타입;
   collapsed: boolean;
   failed: boolean;
-};
+  manual_mode: boolean;
+  default_margin?: 마진셋업타입;
+}
 
-export type 제품정보타입 = {
+export interface 제품정보타입 {
   itemType: 0 | 1 | 2;
   brand: string | undefined;
   product: string | undefined;
@@ -21,9 +23,9 @@ export type 제품정보타입 = {
   margin: number | undefined;
   total_dome: number | undefined;
   soldout: boolean;
-};
+}
 
-export type 배송정보타입 = {
+export interface 배송정보타입 {
   name: string | undefined;
   hp1: string | undefined;
   hp2: string | undefined;
@@ -32,13 +34,13 @@ export type 배송정보타입 = {
   addr3: string | undefined;
   postcode: string | undefined;
   msg: string | undefined;
-};
+}
 
-export type 전체품목리스트 = {
+export interface 전체품목리스트 {
   [key: string]: 개별품목정보[];
-};
+}
 
-export type 개별품목정보 = {
+export interface 개별품목정보 {
   brand: string | undefined;
   brand_kor: string | undefined;
   PROD_CD: string | undefined;
@@ -55,9 +57,23 @@ export type 개별품목정보 = {
   blacklist_user: string | undefined;
   zerostock: number | undefined;
   stock_level: number | undefined;
-};
+  default_margin: 마진셋업타입 | undefined;
+}
 
-export type 임시배열타입 = {
+export interface 개별마진타입 {
+  default_margin: number | string | undefined;
+  default_prov: number | string | undefined;
+  discount_margin: number | string | undefined;
+  discount_price: number | string | undefined;
+  discount_qty: number | string | undefined;
+}
+
+export interface 마진셋업타입 extends 개별마진타입 {
+  per_user: 개별마진타입 | null;
+  brand_disc_amount?: number | string | undefined;
+}
+
+export interface 임시배열타입 {
   product: string | undefined;
   PROD_CD: string | undefined;
   brand: string | undefined;
@@ -66,13 +82,13 @@ export type 임시배열타입 = {
   software: string | undefined;
   stock_level?: number;
   checking?: boolean;
-};
+}
 
-export type 선택상자호출자타입 = {
+export interface 선택상자호출자타입 {
   요소: HTMLElement | undefined;
   인덱스: number;
   품목: 품목리스트항목타입 | undefined;
   유형: string | null | undefined;
-};
+}
 
 export const 배송형태종류 = ["익일수령택배", "방문수령", "퀵착불", "퀵선불", "대리배송", "전자배송", ""] as const;
